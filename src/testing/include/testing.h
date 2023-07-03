f8c0b83bd42085d87dd7d79e154c4428b3b85c72
Jakub Proboszcz <pleban2003@wp.pl>
2023-07-03 13:29:21 +0200

    Implemented basic testing framework

---
+++ src/testing/include/testing.h
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+#include "error_handling.h"
+
+#define COLOR_RESET "\x1B[0m"
+#define COLOR_RED "\x1B[31m"
+#define COLOR_GREEN "\x1B[32m"
+#define COLOR_YELLOW "\x1B[33m"
+#define COLOR_BLUE "\x1B[34m"
+#define COLOR_MAGENTA "\x1B[35m"
+#define COLOR_CYAN "\x1B[36m"
+#define COLOR_WHITE "\x1B[37m"
+
+
+void doTesting(char *fileName, unsigned numberOfFunctions, ...);
+
+
+#define NUMARGS(...)  (sizeof((ReturnCode (*[])(void)){__VA_ARGS__}))/(sizeof(ReturnCode (*)(void)))
+#define PREPARE_TESTING(function_name, ...)  void function_name(void) { \
+    doTesting(__FILE__, NUMARGS(__VA_ARGS__), __VA_ARGS__); \
+}
+
+#define ASSERT(expression) if(!(expression)) { \
+    printf(COLOR_RED "%s(%s:%d) Assertion failed\n" COLOR_RESET, __func__, __FILE__, __LINE__); \
+    return RET_ERROR; \
+}
+#define ASSERT_MESSAGE(expression, message) if(!(expression)) { \
+    printf(COLOR_RED "%s(%s:%d) Assertion failed: %s\n" COLOR_RESET, __func__, __FILE__, __LINE__, (message)); \
+    return RET_ERROR; \
+}
+#define ASSERT_ENSURE(expression) if((expression) != RET_SUCCESS) { \
+    printf(COLOR_RED "%s(%s:%d) Function %s failed\n" COLOR_RESET, __func__, __FILE__, __LINE__, #expression); \
+    return RET_ERROR; \
+}
+#define ASSERT_NOTHROW(expression) if((expression) == RET_ERROR) { \
+    printf(COLOR_RED "%s(%s:%d) Function %s threw error\n" COLOR_RESET, __func__, __FILE__, __LINE__, #expression); \
+    return RET_ERROR; \
+}
+#define ASSERT_THROW(expression) if((expression) != RET_ERROR) { \
+    printf(COLOR_RED "%s(%s:%d) Function %s did not throw error\n" COLOR_RESET, __func__, __FILE__, __LINE__, #expression); \
+    return RET_ERROR; \
+}
---
+++ src/testing/testing.c
@@ -0,0 +1,22 @@
+#include "testing.h"
+#include "logging.h"
+
+
+void doTesting(char *fileName, unsigned numberOfFunctions, ...)
+{
+    logging_level = LOG_SILENT;
+    printf("Testing of file %s\n", fileName);
+    va_list functions;
+    va_start(functions, numberOfFunctions);
+    unsigned succeeded = 0;
+    for(unsigned i = 0; i < numberOfFunctions; i++)
+    {
+        ReturnCode (*testFunction)(void) = va_arg(functions, ReturnCode (*)(void));
+        if(testFunction() == RET_SUCCESS)
+            ++succeeded;
+    }
+    va_end(functions);
+    printf("finished, ");
+    printf(succeeded == numberOfFunctions ? COLOR_GREEN : COLOR_RED);
+    printf("%d/%d tests succeeded.\n" COLOR_RESET, succeeded, numberOfFunctions);
+}
